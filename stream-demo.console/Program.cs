0d4eb41c431825910bb73311140e22b1f77ff298
Doug Stowell <doug.stowell@gmail.com>
2020-04-10 17:46:09 +0100

    Initial commit

---
+++ stream-demo.console/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Tweetinvi;
+using dotenv.net;
+
+namespace StreamDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DotEnv.Config();
+
+            // add your Twitter API credentials here
+            Auth.SetUserCredentials(
+                Environment.GetEnvironmentVariable("CONSUMER_KEY"),
+                Environment.GetEnvironmentVariable("CONSUMER_SECRET"),
+                Environment.GetEnvironmentVariable("USER_ACCESS_TOKEN"),
+                Environment.GetEnvironmentVariable("USER_ACCESS_SECRET")
+            );
+
+            var stream = Stream.CreateFilteredStream();
+            var term = "COVID";
+            stream.AddTrack(term);
+            stream.MatchingTweetReceived += (sender, tweet) =>
+            {
+                Console.WriteLine(
+                    $"A tweet containing { term } has been found; the tweet is { tweet.Tweet }"
+                );
+            };
+            stream.StartStreamMatchingAllConditions();
+        }
+    }
+}
