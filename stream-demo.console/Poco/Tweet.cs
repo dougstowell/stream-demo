acfbb1411113569de99a63ad70e7a5d9f00bf228
Doug Stowell <doug.stowell@gmail.com>
2020-04-12 15:26:54 +0100

    Further dev - wired up to EH

---
+++ stream-demo.console/Poco/Tweet.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using Tweetinvi.Models;
+
+namespace StreamDemo
+{
+    internal class StreamTweet
+    {
+        public StreamTweet(ITweet original)
+        {
+            Id = original.Id;
+            FullText = Regex.Replace(original.FullText, @"(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ").ToString();
+            CreatedAt = original.CreatedAt;
+            User = original.CreatedBy.Name;
+            UserDescription = original.CreatedBy.Description;
+            FollowersCount = original.CreatedBy.FollowersCount;
+
+            var coords = original.Place.BoundingBox.Coordinates.First();
+            Latitude = coords.Latitude;
+            Longitude = coords.Longitude;
+            Country = original.Place.Country;
+        }
+
+
+        public long Id { get; set; }
+
+        public string FullText { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public string User { get; set; }
+
+        public string UserDescription { get; set; }
+
+        public int FollowersCount { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public string Country { get; set; }
+
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize<StreamTweet>(this);
+        }
+    }
+}
--- stream-demo.console/Program.cs
+++ stream-demo.console/Program.cs
@@ -1,33 +1,92 @@
 ﻿using System;
 using Tweetinvi;
 using dotenv.net;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventHubs;
+using System.Text;
+using Tweetinvi.Models;
+using System.Linq;
 
 namespace StreamDemo
 {
     class Program
     {
-        static void Main(string[] args)
+        private static EventHubClient _eventHubClient = null;
+
+
+        /// <summary>
+        /// Main program entry point - async.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <returns>Result of the async call.</returns>
+        private static async Task MainAsync(string[] args)
         {
             DotEnv.Config();
 
-            // add your Twitter API credentials here
-            Auth.SetUserCredentials(
-                Environment.GetEnvironmentVariable("CONSUMER_KEY"),
-                Environment.GetEnvironmentVariable("CONSUMER_SECRET"),
-                Environment.GetEnvironmentVariable("USER_ACCESS_TOKEN"),
-                Environment.GetEnvironmentVariable("USER_ACCESS_SECRET")
-            );
-
-            var stream = Stream.CreateFilteredStream();
-            var term = "COVID";
-            stream.AddTrack(term);
-            stream.MatchingTweetReceived += (sender, tweet) =>
+            try
             {
-                Console.WriteLine(
-                    $"A tweet containing { term } has been found; the tweet is { tweet.Tweet }"
+                // Set up twitter connection.
+                Auth.SetUserCredentials(
+                    Environment.GetEnvironmentVariable("TWITTER_CONSUMER_KEY"),
+                    Environment.GetEnvironmentVariable("TWITTER_CONSUMER_SECRET"),
+                    Environment.GetEnvironmentVariable("TWITTER_USER_ACCESS_TOKEN"),
+                    Environment.GetEnvironmentVariable("TWITTER_USER_ACCESS_SECRET")
                 );
-            };
-            stream.StartStreamMatchingAllConditions();
+
+                // Create hub client.
+                var ehConnStr = new EventHubsConnectionStringBuilder(
+                    Environment.GetEnvironmentVariable("EH_CONN_STR")
+                ).ToString();
+                _eventHubClient = EventHubClient.CreateFromConnectionString(ehConnStr);
+
+                var stream = Stream.CreateFilteredStream();
+                
+                stream.AddTweetLanguageFilter(LanguageFilter.English);
+
+                var track = "COVID";
+                stream.AddTrack(track);
+
+                stream.MatchingTweetReceived += async (sender, data) =>
+                {
+                    try
+                    {
+                        if (data.Tweet.Place != null)
+                        {
+                            var tweetPayload = new StreamTweet(data.Tweet);
+                            await _eventHubClient.SendAsync(
+                                new EventData(Encoding.UTF8.GetBytes(tweetPayload.ToString()))
+                            );
+
+                            Console.WriteLine(
+                                $"A tweet has been SENT; the tweet is { data.Tweet }"
+                            );
+                        }
+
+                    }
+                    catch (Exception exSend)
+                    {
+                        Console.WriteLine($"Error in send - { exSend.Message }");
+                    }
+                };
+                stream.StartStreamMatchingAllConditions();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                await _eventHubClient.CloseAsync();
+            }
+        }
+
+        /// <summary>
+        /// Main program entry point.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
         }
     }
 }
